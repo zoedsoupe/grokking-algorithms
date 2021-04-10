5dd9843f4b29785bda63067554e6a5860cbd6567
Zoey <zoey.spessanha@zeetech.io>
2021-04-10 16:01:55 -0300

    binary search spec corrected

--- spec/bs.test.ts
+++ spec/bs.test.ts
@@ -1,17 +1,13 @@
 import { binarySearch } from "../src/ch1/bs.ts";
-import { quickSort } from "./src/ch4/qsort.ts";
-import {
-  assert,
-  assertStrictEquals,
-} from "https://deno.land/std@0.92.0/testing/asserts.ts";
+import { quickSort } from "../src/ch4/qsort.ts";
+import { assertStrictEquals } from "https://deno.land/std@0.92.0/testing/asserts.ts";
 
 Deno.test("Test Binary Search algorithm", () => {
   const xs: ReadonlyArray<number> = [1, 5, 3, 6, 7, 10, 56, 23, 45, 2];
 
   const ys = [...x].sort();
 
-  const index = binarySearch(ys, 6);
+  const index = binarySearch(ys, () => 6);
 
-  assert(index);
-  assertStrictEquals(index, 4);
+  assertStrictEquals(index(), 4);
 });
