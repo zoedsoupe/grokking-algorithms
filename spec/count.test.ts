a134a37523a82ca0038316bccdda57143aa07cbe
Zoey <zoey.spessanha@zeetech.io>
2021-04-10 16:19:06 -0300

    countL and countA refactored

--- spec/count.test.ts
+++ spec/count.test.ts
@@ -1,10 +1,24 @@
-import { count } from "../ch4/count";
+import { List } from "../src/utils.ts";
+import { countA, countL } from "../ch4/count";
+import { assertStrictEquals } from "https://deno.land/std@0.92.0/testing/asserts.ts";
 
-describe("test count function", () => {
-  it("should return correct number of elements", () => {
-    const arrs: readonly number[][] = [[], [1], [1, 2], [1, 2, 3, 4, 5, 6]];
-    const results = [0, 1, 2, 6];
+Deno.test("countA function", () => {
+  const arrs: readonly number[][] = [[], [1], [1, 2], [1, 2, 3, 4, 5, 6]];
+  const results = [0, 1, 2, 6];
 
-    arrs.forEach((arr, i) => expect(count(arr)).toEqual(results[i]));
-  });
+  const counts = arrs.map((arr) => countA(arr));
+
+  counts.forEach((c, i) => assertStrictEquals(c(), results[i]));
+});
+
+Deno.test("countL function", () => {
+  const xs = toList([]);
+  const ys = toList([1]);
+  const ws = toList([1, 2]);
+  const zs = toList([1, 2, 3, 4, 5, 6]);
+
+  assertStrictEquals(countL(xs)(), 0);
+  assertStrictEquals(countL(ys)(), 1);
+  assertStrictEquals(countL(ws)(), 2);
+  assertStrictEquals(countL(zs)(), 6);
 });
--- src/ch4/count.ts
+++ src/ch4/count.ts
@@ -1,4 +1,27 @@
 // COUNT ELEMENTS ON ARRAY
+import { LazyT, ListT } from "../utils.ts";
 
-export const count = (arr: ReadonlyArray<number>, a = 0): number =>
-  arr.length < 1 ? a : count(arr.slice(1), a + 1);
+export function countA(arr: ReadonlyArray<number>): LazyT<number> {
+  return doCountA(arr, () => 0);
+}
+
+function doCountA(
+  arr: ReadonlyArray<number>,
+  acc: LazyT<number>
+): LazyT<number> {
+  if (arr.length < 1) return acc;
+
+  return doCountA(arr.slice(1), () => acc() + 1);
+}
+
+export function countL(xs: List<number>): LazyT<number> {
+  return doCountL(xs, () => 0);
+}
+
+function doCountL(xs: ListT<number>, acc: LazyT<number>): LazyT<number> {
+  const pair = xs();
+
+  if (pair === null) return 0;
+
+  return doCountL(pair.tail, () => acc() + 1);
+}
